4b81e51754d80791db09563f4d87b7ad95b5348f
Redeate Seife <99093394+redeateseife@users.noreply.github.com>
2026-06-22 11:07:46 -0400

    Refactor AudioEngine.java for improved readability

--- src/AudioEngine.java
+++ src/AudioEngine.java
@@ -1,13 +1,3 @@
-// AudioEngine.java
-
-/**
- * AudioEngine provides sound to JavaKeys.
- *
- * @author Redeate Seife
- * @since 2026-05-15
- * @version 2.0
- */
-
 import java.io.*;
 import java.util.*;
 import javax.sound.sampled.*;
